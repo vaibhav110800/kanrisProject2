93fb760c8c44b0d11a104668c81e7a5a8e7a0c46
vaibhav110800 <vibhu1108@gmail.com>
2020-09-30 21:28:22 +0530

    commit 17
    app.js update

--- js/app.js
+++ js/app.js
@@ -1,7 +1,7 @@
 document.getElementById('1').onclick = function changeContent() {
     document.getElementById('c1').style = "display: block";
     document.getElementById('c2').style = "display: none";
-    document.getElementById('courses3').style = "display: none";
+    document.getElementById('c3').style = "display: none";
 }
 
 document.getElementById('2').onclick = function changeContent() {
