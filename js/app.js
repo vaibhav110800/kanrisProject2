6c9b2bb2c2df826551579f3a552f656d84c499df
vaibhav110800 <vibhu1108@gmail.com>
2020-10-09 22:57:06 +0530

    commit 19
    added search to project page

--- js/app.js
+++ js/app.js
@@ -15,3 +15,18 @@ document.getElementById('3').onclick = function changeContent() {
     document.getElementById('c2').style = "display: none";
     document.getElementById('c3').style = "display: block";
 }
+
+function search_products() {
+    let input = document.getElementById('searchbar').value
+    input = input.toLowerCase();
+    let x = document.getElementsByClassName('pro-box-box');
+
+    for (i = 0; i < x.length; i++) {
+        if (!x[i].innerHTML.toLowerCase().includes(input)) {
+            x[i].style.display = "none";
+        }
+        else {
+            x[i].style.display = "flex";
+        }
+    }
+} 
